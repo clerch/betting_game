d588818702f166aa67112c9c6a29b0410316bfa6
clerch <lerchc@gmail.com>
2016-05-31 10:14:47 -0700

    better implementation of jQuery

--- betting_game.js
+++ betting_game.js
@@ -6,67 +6,48 @@ var money = 100;
 
 
 $("start-new").click(function(e){
-  e.preventDefault();
+  // e.preventDefault();
   money = 100;
+  play_game();
   //trigger game starting
-  });
+});
 
 $("#play-submit").click(function(e){
   e.preventDefault();
-  var bet = document.getElementById("bet").value;
-  var number = document.getElementById("number").value;
-  $("#number_selected").text(number);
-  var random_number = Math.floor((Math.random() * 10) + 1);
-  $("#random_number").text(random_number);
-  var winnings = check_number(random_number, number, bet);
-  $("#turn_winnings").text(winnings);
-  money = money + winnings;
-  $("#money_balance").text(money);
-  game_over(money);
+  play_game();
 });
 
 //initialize the game
 
-function game_over(money){
-  if (money <= 0){
-    console.log("Thanks for the money!");
-  }
-}
-
-function prompt_bet(){
-  var bet = prompt("Please place a bet between $5 and $10");
-  while (bet > 10 || bet < 5){
-    bet = prompt("Incorrect entry: Please place a bet between $5 and $10");
-  }
-  return bet;
-}
-
-// function validate_bet(bet){
-
-//   if (bet <= 10 && bet >= 5){
-//     return bet;
-//   } else {
-//     return prompt_bet();
-//   }
-// }
-
-function prompt_number(bet){
-
-  var number = prompt("You are betting " + bet + ". Please pick a number between 1 and 10.");
+function play_game(){
+  var number = document.getElementById("number").value;
   while (number > 10 || number < 1){
     number = prompt("Incorrect entry: Please pick a number between 1 and 10.");
   }
-  return number;
+
+
+  var bet = document.getElementById("bet").value;
+  while (bet > 10 || bet < 5){
+    bet = prompt("Incorrect entry: Please place a bet between $5 and $10");
+  }
+
+
+  if (money > 0 && bet <= money){
+      
+      $("#number_selected").text(number);
+
+      var random_number = Math.floor((Math.random() * 10) + 1);
+      $("#random_number").text(random_number);
+
+      var winnings = check_number(random_number, number, bet);
+      $("#turn_winnings").text(winnings);
+
+      money = money + winnings;
+      $("#money_balance").text(money);
+      } else {
+    prompt("Thanks for the money!");
+  }
 }
-
-// function validate_number(number,bet){
-
-//     if (number <= 10){
-//       return number;
-//     } else {
-//       return prompt_number(bet);
-//     }
-// }
 
 function check_number(random_number,number, bet){
   if (number == random_number){
@@ -82,8 +63,48 @@ function check_number(random_number,number, bet){
 }
 
 
-
 });
+
+
+
+
+
+// function prompt_bet(){
+//   var bet = prompt("Please place a bet between $5 and $10");
+//   while (bet > 10 || bet < 5){
+//     bet = prompt("Incorrect entry: Please place a bet between $5 and $10");
+//   }
+//   return bet;
+// }
+
+// function validate_bet(bet){
+
+//   if (bet <= 10 && bet >= 5){
+//     return bet;
+//   } else {
+//     return prompt_bet();
+//   }
+// }
+
+// function prompt_number(bet){
+
+  //   var number = prompt("You are betting " + bet + ". Please pick a number between 1 and 10.");
+  //   while (number > 10 || number < 1){
+  //     number = prompt("Incorrect entry: Please pick a number between 1 and 10.");
+  //   }
+  //   return number;
+  // }
+
+// function validate_number(number,bet){
+
+//     if (number <= 10){
+//       return number;
+//     } else {
+//       return prompt_number(bet);
+//     }
+// }
+
+
 
 
 
